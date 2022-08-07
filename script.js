0d1b08c14bcc889bf8f601e5dea6154939978e2b
minarul hoque <minarulak88@gmail.com>
2022-08-07 15:17:28 +0530

    working on rendering details

---
+++ script.js
@@ -0,0 +1,93 @@
+const container = document.querySelector(".container");
+const preloader = document.querySelector(".preloader");
+const searchBtn = document.querySelector(".search_btn");
+const searchBar = document.querySelector(".search_box");
+
+let click = new Audio("./src/click.mp3");
+let audio = new Audio("./src/sound1.mp3");
+document.addEventListener("click", () => {
+  click.play();
+});
+document.addEventListener("keydown", () => {
+  var press = new Audio("./src/press1.mp3");
+  press.play();
+});
+let prev;
+container.addEventListener("mouseover", (e) => {
+  let hover = new Audio("./src/hover.mp3");
+  if (e.target.closest(".box")) {
+    if (prev != e.target.closest(".box")) {
+      hover.play();
+      prev = e.target.closest(".box");
+    }
+  }
+});
+const render = (countries) => {
+  countries = countries.sort((a, b) =>
+    a.name.common > b.name.common ? 1 : b.name.common > a.name.common ? -1 : 0
+  );
+  let allElm = "";
+  countries.forEach((data) => {
+    const html = `<a href="./coutnry.html?name=${data.name.common}" class="box">
+    <div class="top_design"></div>
+    <div class="img_box"><img src="${data.flags.png}" alt=""></div>
+    <div class="details">
+    <div class="name">${data.name.common}</div>
+    <div class="capital">
+    <label>Capital -</label>
+    <span class="capital_name">${data.capital}</span>
+        </div>
+        <div class="continents">
+            <label>Continents -</label>
+            <span class="continents_name">${data.continents[0]}</span>
+        </div>
+        <div class="population">
+            <label>Population -</label>
+            <span class="population_name">${(
+              data.population / 10000000
+            ).toFixed(4)}CR</span>
+            </div>
+            </div>
+    </a>`;
+    allElm += html;
+  });
+  container.innerHTML += allElm;
+  preloader.classList.add("hide");
+};
+const getCourntryByname = (name) => {
+  fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.status == 404) {
+        alert("no country found")
+        searchBar.value = "";
+        getAll();
+        return;
+      }
+      click.play();
+      render(data)
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+};
+const getAll = () => {
+  fetch(`https://restcountries.com/v3.1/all`)
+    .then((res) => res.json())
+    .then((data) => {
+      audio.play();
+      render(data);
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+};
+getAll();
+
+searchBtn.addEventListener("click", () => {
+  let value = searchBar.value;
+  container.innerHTML = "";
+  preloader.classList.remove("hide");
+  getCourntryByname(value);
+});
---
+++ src/country.js
@@ -0,0 +1,79 @@
+const flag = document.querySelector(".flag");
+const coatOfArms = document.querySelector(".coat_arms");
+const mapLink = document.querySelector(".map .title");
+const time = document.querySelector(".time .live_time");
+const altSpeling = document.querySelector(".alt_speling .value");
+const area = document.querySelector(".area .value");
+const borders = document.querySelector(".borders .value");
+const container = document.querySelector(".content")
+const preloader = document.querySelector(".preloader");
+
+const params = new URLSearchParams(window.location.search);
+const countryName = params.get("name");
+document.title = countryName;
+
+const intro = new Audio("./../src/intro.mp3");
+
+const render = ([country]) => {
+  flag.querySelector("img").src = country?.flags?.png;
+  flag.querySelector(".title").textContent ="flag of "+ country?.name.common;
+  coatOfArms.querySelector("img").src = country?.coatOfArms?.png;
+  mapLink.href = country.maps?.openStreetMaps;
+  let offset = country.timezones[0].replace('UTC','').replace(':','.');
+  offset = +offset
+  if(offset>+(offset.toFixed())){
+    offset = '+'+offset.toFixed()+'.50';
+  }
+  setInterval(()=>{
+    let date = calcTime(offset)
+    date = date.slice(date.indexOf(',')).trim();
+    date = date.replace(",","")
+    time.innerHTML= date;
+  },1000)
+  console.log(country);
+
+  altSpeling.innerHTML = country.altSpellings.join(", ")
+  area.innerHTML = country?.area + " KM<sup>2</sup>";
+  borders.innerHTML = country?.borders?.join(", ") == undefined?"":country?.borders?.join(", ");
+
+  preloader.classList.add("hide");
+  intro.play();
+};
+
+const getCourntryByname = (name) => {
+  fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+    .then((res) => res.json())
+    .then((data) => {
+      render(data);
+    });
+
+};
+getCourntryByname(countryName);
+
+function calcTime(offset) {
+  var d = new Date();
+  var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+  var nd = new Date(utc + (3600000*offset));
+  return nd.toLocaleString();
+}
+
+
+// sounds
+let click = new Audio("./src/click.mp3");
+document.addEventListener("click", () => {
+  click.play();
+});
+document.addEventListener("keydown", () => {
+  var press = new Audio("./src/press1.mp3");
+  press.play();
+});
+let prev;
+container.addEventListener("mouseover", (e) => {
+  let hover = new Audio("./src/hover.mp3");
+  if (e.target.closest(".info")) {
+    if(prev != e.target.closest(".info")){
+      hover.play();
+      prev = e.target.closest(".info");
+    }
+  }
+});
